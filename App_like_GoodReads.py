ed8cfb45bb67b95a92bc5c96d8dfb1dae690e8ee
cojoceacristian <Techdeck01>
2021-04-13 20:15:35 +0300

    First step

---
+++ App_like_GoodReads.py
@@ -0,0 +1,22 @@
+#When starting the app, users are provided with a menu
+# to choose whether they want to list, add, edit or share a book
+#When the user starts the app next time, the stored books should be preserved (via csv)
+
+#Step 1: Main menu
+option = print(input("""Welcome reader to
+       Cristian's Cojocea app for your favorite books to keep
+       
+       The main menu commands are:
+       
+1 represents 'Add a book' feature
+2 represents 'List of books' feature
+3 represents 'Update a book' feature
+4 represents 'Share your review for a book'
+
+       Please choose an option -> """))
+#print("""
+#1 represents 'Add a book' feature
+#2 represents 'List of books' feature
+#3 represents 'Update a book' feature
+#4 represents 'Share your review for a book'
+#""")
